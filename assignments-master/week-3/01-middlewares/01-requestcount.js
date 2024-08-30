e1703295b0a402a0bc29088b6af13f8ddd8984b2
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-08-30 13:44:28 +0530

    01- middleware global middleware assignment solved

--- assignments-master/week-3/01-middlewares/01-requestcount.js
+++ assignments-master/week-3/01-middlewares/01-requestcount.js
@@ -1,5 +1,3 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 
 const app = express();
@@ -10,16 +8,23 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+app.use(function(req,res,next){
+  requestCount=requestCount+1;
+  next();
+})
+
 app.get('/user', function(req, res) {
+  requestCount=requestCount+1;
   res.status(200).json({ name: 'john' });
 });
 
 app.post('/user', function(req, res) {
+  requestCount=requestCount+1;
   res.status(200).json({ msg: 'created dummy user' });
 });
 
 app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
-
+ app.listen(3000);
 module.exports = app;
